2da65ad0649c54e6929e7656d256bfd57e9f4ae7
Ngoc Phan <nphan4@yahoo.com>
2018-05-21 10:35:22 -0600

    Seed test env

--- db/seeds/test/test.js
+++ db/seeds/test/test.js
@@ -1,13 +1,14 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
-};
+  return (knex('photos').del()
+    .then(() => {
+      return Promise.all([
+        knex('photos').insert([
+        {name: 'photo 1', url: 'https://www.popsci.com/sites/popsci.com/files/styles/1000_1x_/public/images/2017/10/terrier-puppy.jpg?itok=rIgh3ArV&fc=50,50'},
+        {name: 'photo 2', url: 'https://www.cesarsway.com/sites/newcesarsway/files/styles/large_article_preview/public/The-stages-of-puppy-growth.jpg?itok=9ptPJwY8'},
+        {name: 'photo 3', url: 'https://gfp-2a3tnpzj.stackpathdns.com/wp-content/uploads/2016/07/Dachshund-600x600.jpg'}
+      ], 'id')
+  .then(() => console.log('Seeding Success!'))
+  .catch((error) => console.log(`Error Seeding data: ${error}`))
+    ])
+  }))
+}
