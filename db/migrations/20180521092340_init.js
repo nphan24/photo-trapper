d44161901d458b37b9cf233072d6095de2a9db73
Ngoc Phan <nphan4@yahoo.com>
2018-05-21 09:34:28 -0600

    Added migrations to db

--- db/migrations/20180521092340_init.js
+++ db/migrations/20180521092340_init.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('photos', function(table) {
+      table.increments('id').primary()
+      table.string('name')
+      table.string('url')
+      table.timestamps(true, true)
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('photos')
+  ]);
 };
