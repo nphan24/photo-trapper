c0a5385143f6c464263633309e862a62c79f0bc4
Ngoc Phan <nphan4@yahoo.com>
2018-05-21 12:47:57 -0600

    Tested get and post endpoints

---
+++ test/test.js
@@ -0,0 +1,82 @@
+const chai = require('chai');
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const { app, db } = require('../server');
+
+chai.use(chaiHttp);
+
+describe('client routes', () => {
+  it('should return the homepage', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        done();
+      });
+  })
+
+  it('should return 404 for a route that does not exist', (done) => {
+    chai.request(app)
+      .get('/puppies')
+      .end((error, response) => {
+        response.should.status(404);
+        done();
+      });
+  });
+});
+
+describe('Testing endpoints', () => {
+  beforeEach(done => {
+    db.migrate.rollback()
+      .then(()=> {
+        db.migrate.latest()
+          .then(()=> {
+            return db.seed.run()
+              .then(() => {
+                done();
+              });
+          });
+      });
+  });
+
+  it('GET all photos', (done) => {
+    chai.request(app)
+      .get('/api/v1/photos')
+      .end((error, response) => {
+        response.should.be.json;
+        response.should.have.status(200);
+        response.body.should.be.an('array');
+        response.body.length.should.equal(3);
+        response.body[0].should.have.property('name');
+        response.body[0].should.have.property('url');
+        response.body[0].should.have.property('id');
+        response.body[0].name.should.equal('photo 1');
+        response.body[0].id.should.equal(1);
+        response.body[1].name.should.equal('photo 2');
+        response.body[1].id.should.equal(2);
+        response.body[2].name.should.equal('photo 3');
+        response.body[2].id.should.equal(3);
+        done();
+      });
+  });
+
+  it('POST photos to the database', (done) => {
+    chai.request(app)
+      .post('/api/v1/photos')
+      .send({
+        'name': 'photo 4', 
+        'url': 'http://www.lazerhorse.org/wp-content/uploads/2014/06/Pomeranian-Puppy-cute-clipped.jpg'
+      })
+      .end((error, response) => {
+        response.should.be.json;
+        response.should.have.status(201);
+        response.body.should.be.an('object');
+        response.body.should.have.property('id');
+        response.body.id.should.equal(4);
+        done();
+      });
+  });
+
+
+});
